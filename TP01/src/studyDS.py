c1b65a677043d79fa493253d3da8feaabb498933
Diogo Miguel Silva Araújo <61635272+DMdSA@users.noreply.github.com>
2022-03-19 19:10:12 +0000

    main file updated with examples

--- TP01/src/studyDS.py
+++ TP01/src/studyDS.py
@@ -10,10 +10,6 @@ genderAge = genderAgeQueries.getGenderDetails(emdDS[0])
 #print(genderAge[1])
 #print(genderAge)
 
-genderDetails = genderAgeQueries.calculateGenderDetails(genderAge[0])
-##for a in genderAge[1]:
-##    for emdd in (genderAge[1])[a]:
-##        print(emdd)
 
 
 modalidades01 = modalities.getModalidades01(emdDS[0])
@@ -25,3 +21,7 @@ print(modalidades02)
 
 #modalidadesPerYear = modalities.getModalidadeInfo01(modalidades02, "2020", "Futebol")
 #modalidadesPerModal = modalities.getModalidadeInfo02(modalidades02, "Andebol")
+
+
+alineaB = genderAgeQueries.calculateGenderDetails(genderAge)
+#print(alineaB[1])
