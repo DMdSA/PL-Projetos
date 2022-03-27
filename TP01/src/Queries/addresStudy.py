f29621162463b1928dbb3f609b5a130cef23ff68
Diogo Rebelo <diogolimarebelo@gmail.com>
2022-03-27 17:38:31 +0100

    Update addresStudy.py

--- TP01/src/Queries/addresStudy.py
+++ TP01/src/Queries/addresStudy.py
@@ -97,5 +97,4 @@ def createBarGraphAdress(addresses):
     plt.ylabel("Número de Registos")
     plt.title("10 Cidades Com Mais Registos")
     plt.legend()
-    #plt.show()
     fig.savefig('Images/address_Bar_Graph.png')
