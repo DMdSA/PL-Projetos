197c89783c1cf1857b4771eab337d178644b8703
JoelCAraujo <Joel_araujo@live.com>
2022-05-14 17:58:53 +0100

    Better print display

--- TP02/src/website/views.py
+++ TP02/src/website/views.py
@@ -54,7 +54,7 @@ def htmlTokenizer(line):
     if tokens:
         for tok in tokens:
             if tok[0] == "PYTHON":
-                answer = answer + "<br>Probably python code...: " + str(tok)
+                answer = answer + "Probably python code ... : " + str(tok) + "\n"
             else:
-                answer = answer + str(tok)
+                answer = answer + str(tok) +"\n"
     return answer
