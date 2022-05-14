ac8cd91fad33aff4f8c694136cf094fd92c9b270
JoelCAraujo <Joel_araujo@live.com>
2022-05-14 05:06:28 +0100

    organizing html files

--- TP02/src/website/views.py
+++ TP02/src/website/views.py
@@ -1,6 +1,5 @@
 #This file will contain the paths or html can visit
 from flask import Blueprint, render_template, request
-
 #-------------------------------------------------------------------------------
 import sys
 sys.path.append('../')
@@ -13,25 +12,23 @@ tokenizer.build()
 tokenizer.lexer.begin('LEX')
 #-------------------------------------------------------------------------------
 
-
 views = Blueprint('views', __name__)
 
 #Defining views
 #This will be our home
-@views.route('/home')
+@views.route('/')
 def home():
-    return "<h1> TEST </h1>"
+    return render_template("home.html")
 
 @views.route('/token', methods=['GET','POST'])
 def token():
-    token = ""
     if request.method == 'POST':
         inputToken = request.form.get('editorTexto')
         print(inputToken)
         #Processing input text to tokens
         token = htmlTokenizer(inputToken)
-        
-    return render_template("index.html", result = token)
+        return render_template("token.html", result = token)
+    return render_template("token.html")
 
 @views.route('/grammar', methods=['GET','POST'])
 def grammar():
@@ -41,12 +38,11 @@ def grammar():
         print(inputGrammar)
         #Processing input text to grammar
         grammar = inputGrammar + 'RESULTADO ESTA A FUNCIONAR'
-    return render_template("index.html", result = grammar)
+    return render_template("grammar.html", result = grammar)
 
 @views.route('/about')
 def about():
-    return "<h1> about </h1>"
-
+    return render_template("about.html")
 
 
 
