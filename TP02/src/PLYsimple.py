15e6d3582eaa73d98f90be59c7c64681b70adc92
Diogo Rebelo <diogolimarebelo@gmail.com>
2022-05-15 19:16:14 +0100

    update

--- TP02/src/PLYsimple.py
+++ TP02/src/PLYsimple.py
@@ -5,14 +5,15 @@ from website import create_html
 from plySimple import plySimpleObject
 
 if len(sys.argv) == 1:
-    print("#>Error, missing arguments. Type \help to see the available options")
+    print("#>Error, missing arguments. Type /help to see the available options")
+    exit(0)
 
 elif len(sys.argv) == 2:
     if sys.argv[1].lower()== 'web':
         html = create_html()
         html.run(debug=True)
         exit(0)
-    elif sys.argv[1].lower() == '\\help':
+    elif sys.argv[1].lower() == '/help':
         print(".\\PLYsimple.py web")
         print(".\\PLYsimple.py inputFile")
         print(".\\PLYsimple.py inputFile outputFile")
@@ -26,7 +27,8 @@ elif len(sys.argv) == 3:
     outputname = "../outputFiles/" + sys.argv[2] 
 
 else:
-    print("#> Error, too many arguments.  Type \help to see the available options")
+    print("#> Error, too many arguments.  Type /help to see the available options")
+    exit(0)
 
 
 plysimple = plySimpleObject.PlySimple(inputname,outputname)
